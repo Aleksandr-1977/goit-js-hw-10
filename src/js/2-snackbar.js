721b59e9045115c8cc8b792671c78ae1f3adeca0
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-01-11 01:33:28 +0200

    task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
 import iziToast from 'izitoast';
+
+const promiseForm = document.querySelector('.form');
+
+promiseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const delayInput = promiseForm.delay.value;
+  const stateCheck = promiseForm.state.value;
+
+  makePromise(delayInput, stateCheck)
+    .then(delay => {
+      iziToast.success({
+        title: 'Успешно',
+        message: `Промис выполнен c задержкой в ${delay} мсек`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Ошибка',
+        message: `Промис отклонен c задержкой в ${delay} мсек`,
+        position: 'topRight',
+      });
+    });
+  promiseForm.reset();
+});
+function makePromise(delayInput, stateCheck) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateCheck === 'fulfilled') {
+        resolve(delayInput);
+      } else {
+        reject(delayInput);
+      }
+    }, delayInput);
+  });
+}
